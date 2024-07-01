e2edbee7a71d2c3c6f3924211da079b661375c30
Lewis <l.mcdonald@dmnet.uk>
2024-07-01 13:28:22 +0100

    Major overhaul commonjs support dropped for the time being FINALLY A WORKING VERSION

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,12 @@
-import chalk from 'chalk'
+import consoleColours from 'console-log-colors';
 
 export type options = {
-    logLevel?: Level,
-    colours?: colours,
-    suppressWarnings?: boolean
-    quitOnFatal?: boolean
-    expandedFatal?: boolean
+    logLevel?: Level; // default 1
+    suppressWarnings?: boolean;
+    quitOnFatal?: boolean;
+    useBackgroundColours?: boolean;
+    indent?: number
 };
-
-type hex = string
-
-type colours = {
-    trace: hex
-    debug: hex
-    info: hex
-    warn: hex
-    error: hex
-    fatal: hex
-}
 
 enum Level {
     trace = 0,
@@ -29,118 +18,62 @@ enum Level {
 }
 
 export class logger {
-    readonly defaultTraceColour: hex = `#42ecff`
-    readonly defaultDebugColour: hex = `#008504`
-    readonly defaultInfoColour: hex = `#00c1d6`
-    readonly defaultWarnColour: hex = `#ffee00`
-    readonly defaultErrorColour: hex = `#ff5e00`
-    readonly defaultFatalColour: hex = `#ff0000`
-    options: options = {}
+    options: options = {};
+    indent: number = 0
 
     constructor(name: string, options?: options) {
-        if (options?.suppressWarnings === true) {
-            if (name.length > 15) throw new Error("Name invalid. Check the FAQ");
+        let lvlIndent:number = 0;
+
+        if (!options) options = {logLevel:Level.debug}
+        else {
+            if (!options.logLevel === undefined) options.logLevel === Level.debug
+            if (options.indent === undefined) lvlIndent === 0;
+            else this.internalLogging(`Indentation is not available in the current version`)
+        }
+        if (options.suppressWarnings === true) {
+            if (name.length > 15) throw new Error('Name invalid. Check the FAQ');
             const ansiRegex: RegExp = /\x1b\[[0-9;]*m/g;
             const matches = name.match(ansiRegex) ?? [];
-            if (matches.length > 0) throw new Error("ANSI characters in name found.");
-
-            this.options = options
+            if (matches.length > 0) throw new Error('ANSI characters in name found.');
         }
 
-    }
-    /**
-     * Calls the log to output
-     * @internal
-     * @param message The error/log message
-     * @param level The level to log at
-     * @param data Extra data that came along
-     */
-    private async log(message: string, level: Level, ...data: Array<unknown>): Promise<void> {
-        const colours = this.options.colours
-
-        let colour;
-
-        if (level === 5) {
-            if (!colours) colour = this.defaultFatalColour;
-            else colour = colours.fatal;
-
-            if (this.options.expandedFatal) {
-                // @ts-ignore
-                console.log(chalk.hex(colour) + `--- FATAL ERROR --- \n` + chalk.whiteBright + message + `\n` + data)
-            } else {
-                // @ts-ignore
-                console.log(chalk.hex(colour) + `--- FATAL ERROR ---` + chalk.whiteBright + message + data)
-            }
-        } else {
-            if (!colours) {
-                if (level === 0) colour = this.defaultTraceColour;
-                if (level === 1) colour = this.defaultDebugColour;
-                if (level === 2) colour = this.defaultInfoColour;
-                if (level === 3) colour = this.defaultWarnColour;
-                if (level === 4) colour = this.defaultErrorColour;
-            } else {
-                if (level === 0) colour = colours.trace;
-                if (level === 1) colour = colours.debug;
-                if (level === 2) colour = colours.info;
-                if (level === 3) colour = colours.warn;
-                if (level === 4) colour = colours.error;
-            }
-
-            if (!colour) throw new Error(`Colour missing`)
-
-            // @ts-ignore
-            console.log(chalk.hex(colour) + Level[level] + chalk.whiteBright + message + data)
-        }
+        this.options = options
+        this.indent = lvlIndent
     }
 
-    /**
-     * Logs a fatal message to the console
-     * @optional quits the process on call set in options
-     * @param message 
-     * @param data 
-     */
-    public async fatal(message: string, ...data: Array<unknown>): Promise<void> {
-        this.log(message, Level.fatal, data);
-        if (this.options.quitOnFatal) process.exit(1);
+    public trace(message: string, ...data: Array<unknown>) {
+        if (this.options.logLevel && this.options.logLevel > Level.trace) return;
+
+        console.log(consoleColours.blue`[Trace]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
     }
-    /**
-     * Logs a trace to the console
-     * @param message 
-     * @param data 
-     */
-    public async trace(message: string, ...data: Array<unknown>): Promise<void> {
-        this.log(message, Level.trace, data);
+    public debug(message: string, ...data: Array<unknown>) {
+        if (this.options.logLevel && this.options.logLevel > Level.trace) return;
+
+        console.log(consoleColours.cyan`[Debug]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
     }
-    /**
-     * Logs a trace to the console
-     * @param message 
-     * @param data 
-     */
-    public async debug(message: string, ...data: Array<unknown>): Promise<void> {
-        this.log(message, Level.debug, data);
+    public info(message: string, ...data: Array<unknown>) {
+        if (this.options.logLevel && this.options.logLevel > Level.trace) return;
+
+        console.log(consoleColours.blueBright`[Info]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
     }
-    /**
-     * Logs a message to the console
-     * @param message 
-     * @param data 
-     */
-    public async info(message: string, ...data: Array<unknown>): Promise<void> {
-        this.log(message, Level.info, data);
+    public warn(message: string, ...data: Array<unknown>) {
+        if (this.options.logLevel && this.options.logLevel > Level.trace) return;
+
+        console.log(consoleColours.yellow`[Warn]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
     }
-    /**
-     * Logs a warning to the console
-     * @param message 
-     * @param data 
-     */
-    public async warn(message: string, ...data: Array<unknown>): Promise<void> {
-        this.log(message, Level.warn, data);
+    public error(message: string, ...data: Array<unknown>) {
+        if (this.options.logLevel && this.options.logLevel > Level.trace) return;
+
+        console.log(consoleColours.red`[Error]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
     }
-    /**
-     * Logs a error to the console
-     * @param message 
-     * @param data 
-     */
-    public async error(message: string, ...data: Array<unknown>): Promise<void> {
-        this.log(message, Level.error, data);
+    public fatal(message: string, ...data: Array<unknown>) {
+        if (this.options.logLevel && this.options.logLevel > Level.trace) return;
+
+        console.log(consoleColours.redBG`[Fatal]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
+    }
+    private internalLogging(message: string, ...data: Array<unknown>) {
+        if (this.options.logLevel && this.options.logLevel > Level.trace) return;
+
+        console.log(`\n` + consoleColours.greenBright`[Internal]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`) + `\n`);
     }
 }
--- test/common.cjs
+++
@@ -1,9 +0,0 @@
-const logger = require("../dist/index");
-
-const log = new logger("test");
-
-try {
-    log.info("auuuh", { auuuh: { test: 4 }, why: 3 });
-} catch (err) {
-    console.log(err.stack);
-}
