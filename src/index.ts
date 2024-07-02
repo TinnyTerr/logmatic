6b01275f142900510494ee3bfd8fa3a75e5fb921
Lewis McDonald <l.mcdonald@dmnet.uk>
2024-07-02 10:58:40 +0000

    Major release Indentation now functional Time now visible Formatting objects

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import consoleColours from 'console-log-colors';
+import { plugins, format as prettyFormat } from 'pretty-format';
+
+let formatOptions: object;
 
 export type options = {
     logLevel?: Level; // default 1
     suppressWarnings?: boolean;
     quitOnFatal?: boolean;
-    useBackgroundColours?: boolean;
+    format?: boolean;
     indent?: number
 };
 
@@ -17,20 +20,27 @@ enum Level {
     fatal = 5,
 }
 
+function getTime(): string {
+    const now = new Date();
+    const date = new Date(now.getTime() - (now.getTimezoneOffset() * 60000));
+    return date.toISOString().replace(/.*T(.*)Z/, '$1');
+}
+
 export class logger {
+    name: string = ""
     options: options = {};
     indent: number = 0
 
     constructor(name: string, options?: options) {
-        let lvlIndent:number = 0;
+        let lvlIndent: number = 0;
 
-        if (!options) options = {logLevel:Level.debug}
+        if (!options) options = { logLevel: Level.debug }
         else {
             if (!options.logLevel === undefined) options.logLevel === Level.debug
             if (options.indent === undefined) lvlIndent === 0;
-            else this.internalLogging(`Indentation is not available in the current version`)
+            // * NO MORE DO WE HAVE THE INDENTATION WARNING WHOOOOOH
         }
-        if (options.suppressWarnings === true) {
+        if (options.suppressWarnings === false) {
             if (name.length > 15) throw new Error('Name invalid. Check the FAQ');
             const ansiRegex: RegExp = /\x1b\[[0-9;]*m/g;
             const matches = name.match(ansiRegex) ?? [];
@@ -38,38 +48,61 @@ export class logger {
         }
 
         this.options = options
-        this.indent = lvlIndent
+        if (options.indent && options.indent > 0) {
+            formatOptions = {
+                highlight: true,
+                indent: options.indent
+            }
+        } else {
+            formatOptions = {
+                highlight: true,
+                min: true
+            }
+        }
+        this.name = `${name}`
+    }
+
+    log(title: string, message: string, data: Array<unknown> | string) {
+        let name = this.name;
+
+        return console.log(consoleColours.grey(getTime()), consoleColours.white(name), title, consoleColours.reset(message), consoleColours.green(prettyFormat(data, formatOptions)));
     }
 
     public trace(message: string, ...data: Array<unknown>) {
         if (this.options.logLevel && this.options.logLevel > Level.trace) return;
 
-        console.log(consoleColours.blue`[Trace]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
+        this.log(consoleColours.blue`[Trace]`, message, data)
+        // console.log(consoleColours.blue`[Trace]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
     }
     public debug(message: string, ...data: Array<unknown>) {
         if (this.options.logLevel && this.options.logLevel > Level.trace) return;
 
-        console.log(consoleColours.cyan`[Debug]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
+        this.log(consoleColours.cyan`[Debug]`, message, data)
+        // console.log(consoleColours.cyan`[Debug]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
     }
     public info(message: string, ...data: Array<unknown>) {
         if (this.options.logLevel && this.options.logLevel > Level.trace) return;
 
-        console.log(consoleColours.blueBright`[Info]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
+        this.log(consoleColours.blueBright`[Info]`, message, data)
+        // console.log(consoleColours.blueBright`[Info]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
     }
     public warn(message: string, ...data: Array<unknown>) {
         if (this.options.logLevel && this.options.logLevel > Level.trace) return;
 
-        console.log(consoleColours.yellow`[Warn]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
+        this.log(consoleColours.yellow`[Warn]`, message, data)
+        // console.log(consoleColours.yellow`[Warn]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
     }
     public error(message: string, ...data: Array<unknown>) {
         if (this.options.logLevel && this.options.logLevel > Level.trace) return;
 
-        console.log(consoleColours.red`[Error]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
+        this.log(consoleColours.red`[Error]`, message, data)
+        // console.log(consoleColours.red`[Error]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
     }
     public fatal(message: string, ...data: Array<unknown>) {
         if (this.options.logLevel && this.options.logLevel > Level.trace) return;
 
-        console.log(consoleColours.redBG`[Fatal]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
+        this.log(consoleColours.redBG`[Fatal]`, message, data)
+        // console.log(consoleColours.redBG`[Fatal]` + " " + message + " " + consoleColours.green(`${JSON.stringify(data, null, this.indent)}`));
     }
     private internalLogging(message: string, ...data: Array<unknown>) {
         if (this.options.logLevel && this.options.logLevel > Level.trace) return;
